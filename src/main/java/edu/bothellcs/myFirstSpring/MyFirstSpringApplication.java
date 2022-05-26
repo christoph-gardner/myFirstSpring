5a250181f50fc7f15420d0e0332d9b447d2609e6
Christoph Gardner <1083542@apps.nsd.org>
2022-05-26 08:43:15 -0700

    yes

--- src/main/java/edu/bothellcs/myFirstSpring/MyFirstSpringApplication.java
+++ src/main/java/edu/bothellcs/myFirstSpring/MyFirstSpringApplication.java
@@ -2,12 +2,23 @@ package edu.bothellcs.myFirstSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+              
 @SpringBootApplication
+@RestController
 public class MyFirstSpringApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MyFirstSpringApplication.class, args);
-	}
-
-}
+                
+                  
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstSpringApplication.class, args);
+    }
+                  
+        @GetMapping("/hello")
+        public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        	return String.format("Hello %s!", name);
+    	}
+                
+    }
+            
